1a6c6c3522c8f70f129c84647eca4220c5ea9cd1
mbfds <marcelo.florentino.ti@gmail.com>
2018-11-27 12:07:20 -0200

    Trabalho desenvolvido no modulo de logica de programação

---
+++ componentes/Form2.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace componentes
+{
+    partial class FormPessoa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.rbMasculino = new System.Windows.Forms.RadioButton();
+            this.rbFeminino = new System.Windows.Forms.RadioButton();
+            this.cbEstadoCivil = new System.Windows.Forms.ComboBox();
+            this.cbPagode = new System.Windows.Forms.CheckBox();
+            this.cbSertanejo = new System.Windows.Forms.CheckBox();
+            this.cbRock = new System.Windows.Forms.CheckBox();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sexo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Estado Civil:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Resultado:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Gosto Musical :";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(73, 66);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 6;
+            // 
+            // rbMasculino
+            // 
+            this.rbMasculino.AutoSize = true;
+            this.rbMasculino.Location = new System.Drawing.Point(92, 104);
+            this.rbMasculino.Name = "rbMasculino";
+            this.rbMasculino.Size = new System.Drawing.Size(73, 17);
+            this.rbMasculino.TabIndex = 7;
+            this.rbMasculino.Tag = "sexo";
+            this.rbMasculino.Text = "Masculino";
+            this.rbMasculino.UseVisualStyleBackColor = true;
+            // 
+            // rbFeminino
+            // 
+            this.rbFeminino.AutoSize = true;
+            this.rbFeminino.Checked = true;
+            this.rbFeminino.Location = new System.Drawing.Point(226, 109);
+            this.rbFeminino.Name = "rbFeminino";
+            this.rbFeminino.Size = new System.Drawing.Size(67, 17);
+            this.rbFeminino.TabIndex = 8;
+            this.rbFeminino.TabStop = true;
+            this.rbFeminino.Tag = "sexo";
+            this.rbFeminino.Text = "Feminino";
+            this.rbFeminino.UseVisualStyleBackColor = true;
+            // 
+            // cbEstadoCivil
+            // 
+            this.cbEstadoCivil.FormattingEnabled = true;
+            this.cbEstadoCivil.Items.AddRange(new object[] {
+            "Solteiro",
+            "Casado",
+            "Separado"});
+            this.cbEstadoCivil.Location = new System.Drawing.Point(92, 137);
+            this.cbEstadoCivil.Name = "cbEstadoCivil";
+            this.cbEstadoCivil.Size = new System.Drawing.Size(121, 21);
+            this.cbEstadoCivil.TabIndex = 9;
+            // 
+            // cbPagode
+            // 
+            this.cbPagode.AutoSize = true;
+            this.cbPagode.Location = new System.Drawing.Point(110, 181);
+            this.cbPagode.Name = "cbPagode";
+            this.cbPagode.Size = new System.Drawing.Size(63, 17);
+            this.cbPagode.TabIndex = 10;
+            this.cbPagode.Text = "Pagode";
+            this.cbPagode.UseVisualStyleBackColor = true;
+            // 
+            // cbSertanejo
+            // 
+            this.cbSertanejo.AutoSize = true;
+            this.cbSertanejo.Location = new System.Drawing.Point(208, 181);
+            this.cbSertanejo.Name = "cbSertanejo";
+            this.cbSertanejo.Size = new System.Drawing.Size(71, 17);
+            this.cbSertanejo.TabIndex = 11;
+            this.cbSertanejo.Text = "Sertanojo";
+            this.cbSertanejo.UseVisualStyleBackColor = true;
+            // 
+            // cbRock
+            // 
+            this.cbRock.AutoSize = true;
+            this.cbRock.Location = new System.Drawing.Point(295, 184);
+            this.cbRock.Name = "cbRock";
+            this.cbRock.Size = new System.Drawing.Size(52, 17);
+            this.cbRock.TabIndex = 12;
+            this.cbRock.Text = "Rock";
+            this.cbRock.UseVisualStyleBackColor = true;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(86, 223);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.TabIndex = 13;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(240, 223);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 14;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(110, 273);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(207, 77);
+            this.txtResultado.TabIndex = 15;
+            // 
+            // FormPessoa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 450);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.cbRock);
+            this.Controls.Add(this.cbSertanejo);
+            this.Controls.Add(this.cbPagode);
+            this.Controls.Add(this.cbEstadoCivil);
+            this.Controls.Add(this.rbFeminino);
+            this.Controls.Add(this.rbMasculino);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormPessoa";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.RadioButton rbMasculino;
+        private System.Windows.Forms.RadioButton rbFeminino;
+        private System.Windows.Forms.CheckBox cbPagode;
+        private System.Windows.Forms.CheckBox cbSertanejo;
+        private System.Windows.Forms.CheckBox cbRock;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.TextBox txtResultado;
+        protected System.Windows.Forms.ComboBox cbEstadoCivil;
+    }
+}
---
+++ componentes/Form2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace componentes
+{
+    public partial class FormPessoa : Form
+    {
+        public FormPessoa()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            string mensagem = "";
+            //verificar radio button
+            if (rbFeminino.Checked)
+            {
+                mensagem = "Olá Senhora, " + txtNome.Text;
+            }else if (rbMasculino.Checked)
+            {
+                mensagem = "Olá Senhor, " + txtNome.Text;
+            }
+
+            mensagem = mensagem + "Seu estado civil é " + cbEstadoCivil.Text;
+
+            mensagem = mensagem + " voce curte: ";
+            if (cbPagode.Checked)
+            {
+                mensagem = mensagem + " Pagode ";
+            }
+            if (cbRock.Checked)
+            {
+                mensagem = mensagem + " Rock ";
+
+            }if(cbSertanejo.Checked)
+            {
+                mensagem = mensagem + "Sertanejo";
+            }
+            
+
+
+            txtResultado.Text = mensagem;
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+
+            txtNome.Clear();
+            txtResultado.Clear();
+            cbSertanejo.Checked = false;
+            cbRock.Checked = false;
+            cbPagode.Checked = false;
+            cbEstadoCivil.SelectedIndex = 0;
+
+        }
+    }
+}
